86d25ba4eb52586346cced51d14eda731495bba7
lesedi thibile <lesedi.xys11@gmail.com>
2025-07-20 19:32:33 +0200

    FEAT: Database Modeling and Data Seeding in Django

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Listing, Booking
 
 
-class ListingSerializer(serializers.ModelSerializer):
+class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
         fields = '__all__'
@@ -12,3 +12,15 @@ class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
         fields = '__all__'
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    bookings = BookingSerializer(many=True)
+    city = serializers.CharField(source='location_id.city')
+    country = serializers.CharField(source='location_id.country')
+    zip_code = serializers.CharField(source='location_id.zip_code')
+
+    class Meta:
+        model = Listing
+        fields = ['property_id', 'host_id', 'name', 'city',
+                  'country', 'zip_code', 'price_per_night', 'created_at']
